d9276959a9a319c6062ccaeaa428c3e138802e8c
岳全江 <410281036@qq.com>
2024-02-21 16:08:52 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  NEXT_PUBLIC_PROJECT_ID: '98a9f5ca6cfe5819f5ee971f0793ef88'
+  env: {
+    NEXT_PUBLIC_PROJECT_ID: '98a9f5ca6cfe5819f5ee971f0793ef88'
+  }
 }
 
 module.exports = nextConfig
